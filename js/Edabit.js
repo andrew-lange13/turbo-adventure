16d19c17ce708892a2ce014e1f23fa9dd126d243
Andrew Lange <andrew.lange13@gmail.com>
2022-01-22 20:03:09 -0600

    adds 2 completed exercises from edabit

--- js/Edabit.js
+++ js/Edabit.js
@@ -19,3 +19,18 @@ function whichIsLarger(f, g) {
        return "neither"
     }
 }
+
+// Create a function that takes a number as an argument. Add up all the numbers from 1 to the number you passed to the function. For example, if the input is 4 then your function should return 10 because 1 + 2 + 3 + 4 = 10.
+
+function addUp(num) {
+    if (num === 1) return 1;
+    return num + addUp(num - 1);
+}
+
+// Create a function that takes an array of numbers and return both the minimum and maximum numbers, in that order.
+
+function minMax(arr) {
+    let min = Math.min(...arr)
+    let max = Math.max(...arr)
+    return ([min, max])
+}
